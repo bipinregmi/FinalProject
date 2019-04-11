42a67d84cc0771d2424a9c8b11c38857ad6f42cd
Roberto Sandoval <robertosandoval@rhwlb-53-71.sonoma.edu>
2019-04-10 19:03:09 -0700

    work in progress

--- Spotlight_Movies/app/src/main/java/com/example/spotlight_movies/MovieActivity.java
+++ Spotlight_Movies/app/src/main/java/com/example/spotlight_movies/MovieActivity.java
@@ -1,15 +1,38 @@
 package com.example.spotlight_movies;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
 
 public class MovieActivity extends AppCompatActivity {
     private static final String NAME_KEY = "MoviesActivity";
+    private String path ="https://www.youtube.com/watch?v=zK0LNzU2TQI";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
 
+        Uri uri = Uri.parse(path);
+
+        VideoView vv = (VideoView) findViewById(R.id.videoView);
+        vv.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer arg0, int arg1, int arg2) {return false;}
+        });
+        try {vv.setVideoURI(uri);} catch (Exception e) {}
+        try {vv.start();} catch (Exception e) {}
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+                mp.setOnCompletionListener(null);
+            }
+        });
+
     }
+
 }
